f625892ac530b88c36250c1831ad8ce475b85a32
Prince AIM <mgenique.random@hotmail.com>
2025-08-06 19:11:43 +0200

    Initial commit

---
+++ backend/src/main/kotlin/io/github/princeaim/template/HelloController.kt
@@ -0,0 +1,10 @@
+package io.github.princeaim.template
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun index(): String = "Hello World"
+}
